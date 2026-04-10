fffc26935c713ca309d2c3af030c457a4a5ce117
Bhanuka10 <sahanbhanuka10@gmail.com>
2026-04-10 19:20:48 +0530

    middle ware

---
+++ teacher-parent-system/app/Http/Middleware/TeacherMiddleware.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class TeacherMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (!auth()->check() || auth()->user()->role !== 'teacher') {
+            abort(403, 'Access denied.');
+        }
+        return $next($request);
+    }
+}
